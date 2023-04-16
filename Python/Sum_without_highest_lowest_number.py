2f6266f0c755d395422363b9214dfcc64b9e4713
Eduardo Soares Azzolin <eduduazzolin@gmail.com>
2023-04-16 10:56:00 -0300

    Adicionado o desafio Sum without highest and lowest number

---
+++ Python/Sum_without_highest_lowest_number.py
@@ -0,0 +1,37 @@
+'''
+TITLE:
+Sum without highest and lowest number
+
+DESCRIPTION:
+Sum all the numbers of a given list, except the highest and the lowest element (by value, not by index!).
+The highest or lowest element respectively is a single element at each edge,
+even if there are more than one with the same value.
+Mind the input validation.
+Example
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6
+'''
+
+
+def sum_array(arr):
+    if arr is None or len(arr) == 0:
+        return 0
+    if len(arr) == 1:
+        return 0
+
+    # the largest but O(n) way:
+    max_value = arr[0]
+    min_value = arr[0]
+    sum_value = 0
+
+    for n in arr:
+        if n > max_value:
+            max_value = n
+        if n < min_value:
+            min_value = n
+        sum_value += n
+
+    return sum_value - max_value - min_value
+
+    # the easyiest way:
+    # return sum(arr) - max(arr) - min(arr)
