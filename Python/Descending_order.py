cd16b73bdd9b512ded3a7d24437da5c06de9966b
Eduardo Soares Azzolin <eduduazzolin@gmail.com>
2023-04-17 16:53:55 -0300

    Adicionado o desafio Descending Order

---
+++ Python/Descending_order.py
@@ -0,0 +1,22 @@
+'''
+TITLE:
+Descending Order
+
+DESCRIPTION:
+Your task is to make a function that can take any non-negative integer as an argument
+and return it with its digits in descending order. Essentially, rearrange the digits
+to create the highest possible number.
+
+Examples:
+Input: 42145 Output: 54421
+Input: 145263 Output: 654321
+'''
+
+
+def descending_order(num):
+    # this function turns the number into a list of ints
+    # puts the list in descending order
+    # joins the list into a single string and casts it to int
+    number_list = list(map(int, str(num)))
+    number_list.sort(reverse=True)
+    return int(''.join(map(str, number_list)))
