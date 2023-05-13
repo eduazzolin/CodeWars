bbce5d1d455c40ecc17a33921338ef603353481f
Eduardo Soares Azzolin <eduduazzolin@gmail.com>
2023-05-13 19:14:55 -0300

    New java challenge

---
+++ Java/6kyu_BreakCamelCase.java
@@ -0,0 +1,32 @@
+/*
+TITLE:
+Break camelCase
+
+DESCRIPTION:
+Complete the solution so that the function will break up camel casing, using a space
+between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+
+KYU & CREDITS:
+6 kyu hakt
+https://www.codewars.com/kata/5208f99aee097e6552000148
+*/
+
+public class kyu_BreakCamelCase {
+
+   public static String camelCase(String input) {
+      String result = "";
+      for (int i = 0; i < input.length(); i++) {
+         if ((int) input.charAt(i) >= 65 && (int) input.charAt(i) <= 90) {
+            result += " ";
+         }
+         result += input.charAt(i);
+      }
+      return result;
+   }
+
+}
